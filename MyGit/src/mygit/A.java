3e2fe8c06bec3a642cdc59b2d2e43446ae916d3a
ohjw26 <zionyy@skuniv.ac.kr>
2023-12-21 02:02:51 +0900

    Fist commit in devB branch

--- MyGit/src/mygit/A.java
+++ MyGit/src/mygit/A.java
@@ -4,5 +4,7 @@ public class A {
 	public static void main(String[] args) {
 		System.out.println("Hello A!");
 		System.out.println("This si the 2nd version");
+		B your = new B();
+		your.sayHello("Everyone");
 	}
 }
